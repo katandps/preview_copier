6361ef1941bdcbd1970ff8e016ece12b2ef079af
katand <katandps@gmail.com>
2021-01-23 23:52:01 +0900

    sqliteファイルに接続する

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,35 @@
+use diesel::prelude::*;
+use std::collections::HashMap;
+
+#[macro_use]
+extern crate diesel;
+
 fn main() {
-    println!("Hello, world!");
+    let connection =
+        SqliteConnection::establish("./songdata.db").expect("データベースに接続できませんでした");
+
+    use crate::folder::dsl::*;
+    let record: Vec<Record> = folder
+        .load(&connection)
+        .expect("クエリが実行できませんでした");
+
+    let mut map = HashMap::new();
+    for row in record {
+        map.insert(row.title, row.path);
+    }
+
+    dbg!(map.len());
 }
 
+#[derive(Queryable)]
 struct Record {
     title: String,
     path: String,
+}
+
+table! {
+    folder(path) {
+        title -> Text,
+        path -> Text,
+    }
 }
